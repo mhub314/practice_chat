bd42d5600217b3f0e72908eb0ebad08861c24e17
Mary Hubert <mhubert314@gmail.com>
2024-05-14 06:40:13 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# set OPENAI_API_KEY='sk-proj-6AVl81YNIHWjiiiukcANT3BlbkFJwPa53dde4HNTabwwRBdL'
+
+import openai
+openai.api_key = 'sk-proj-uq2Nvp1iBYVCmrRx0dO5T3BlbkFJ8y2f9pUTqU2ypdRTChjf'
+
+def get_recommendation(user_input):
+    prompt =  """
+    Give me 3 movie recommendations like {}.
+    """.format(user_input)
+    response = openai.Completion.create(
+        engine='davinci-002',
+        prompt=prompt,
+        max_tokens=200
+    )
+    return response.choices[0].text.strip()
+
+user_input = input('What would you like a recommendation for?: ' )
+recommendation = get_recommendation(user_input)
+print("Recommendations based on '{}':".format(user_input))
+print(recommendation)
+
+
+# from openai import OpenAI
+# client = OpenAI()
+  
+# assistant = client.beta.assistants.create(
+#   name="Math Tutor",
+#   instructions="You are a personal math tutor. Write and run code to answer math questions.",
+#   tools=[{"type": "code_interpreter"}],
+#   model="gpt-4-turbo",
+# )
+
+# thread = client.beta.threads.create()
+
+# message = client.beta.threads.messages.create(
+#   thread_id=thread.id,
+#   role="user",
+#   content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+# )
+
+
+# run = client.beta.threads.runs.create_and_poll(
+#   thread_id=thread.id,
+#   assistant_id=assistant.id,
+#   instructions="Please address the user as Jane Doe. The user has a premium account."
+# )
+
+
+# if run.status == 'completed': 
+#   messages = client.beta.threads.messages.list(
+#     thread_id=thread.id
+#   )
+#   print(messages)
+# else:
+#   print(run.status)
---
+++ tmdb.py
@@ -0,0 +1,29 @@
+import requests
+
+# Replace 'YOUR_API_KEY' with your actual TMDB API key
+API_KEY = '655e0bd373de4b5e5de75cb11ad54032'
+BASE_URL = 'https://api.themoviedb.org/3'
+
+def get_movie_rating(movie_title):
+    url = f'{BASE_URL}/search/movie'
+    params = {'api_key': "655e0bd373de4b5e5de75cb11ad54032", 'query': movie_title}
+    response = requests.get(url, params=params)
+    data = response.json()
+    if data['results']:
+        movie_id = data['results'][0]['id']
+        url = f'{BASE_URL}/movie/{movie_id}/ratings'
+        params = {'api_key': "655e0bd373de4b5e5de75cb11ad54032"}
+        response = requests.get(url, params=params)
+        ratings_data = response.json()
+        return ratings_data
+    else:
+        return None
+
+# Example usage
+movie_title = 'Inception'
+ratings = get_movie_rating(movie_title)
+if ratings:
+    print(f"Ratings for '{movie_title}': {ratings}")
+else:
+    print(f"No ratings found for '{movie_title}'")
+
